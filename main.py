c93e2f3068a6983d30101cce3840fbac8c58c5c3
Caleb-Shea <shea.number.1@gmail.com>
2021-05-25 13:28:09 -0500

    added zoom

--- main.py
+++ main.py
@@ -20,11 +20,38 @@ class Tile(pyg.sprite.Sprite):
 
     Minimap label
     """
-    def __init__(self, window, color, palette):
+    def __init__(self, window, color, palette, pos, size):
         super().__init__()
         self.window = window
-        self.color = color
+
+        if color == 'black':
+            self.color = (0, 0, 0)
+        else:
+            self.color = (250, 250, 250)
+
         self.palette = palette
+
+        self.size = size
+        self.true_pos = pos
+        self.scaled_pos = self.true_pos
+
+        #self.image = pyg.image.load(get_path('assets', 'imgs', 'tiles', palette, f'{color}.png'))
+        self.image = pyg.Surface((self.size, self.size)).convert()
+        self.image.fill(self.color)
+        self.rect = self.image.get_rect()
+        self.rect.topleft = self.scaled_pos
+        self.draw_rect = self.rect
+
+    def update(self):
+        self.scaled_pos = (self.true_pos[0]*self.size,
+                           self.true_pos[1]*self.size)
+        self.rect.topleft = self.scaled_pos
+        self.image = pyg.Surface((self.size, self.size)).convert()
+        self.image.fill(self.color)
+
+    def render(self):
+        if self.draw_rect.colliderect(window_rect):
+            self.window.blit(self.image, self.draw_rect)
 
 class Board():
     """
@@ -43,7 +70,7 @@ class Board():
 
         self.tiles = pyg.sprite.Group()
 
-    def generate(self, size=(8, 8), palette='default'):
+    def generate(self, size=(100, 100), palette='default'):
         for y in range(size[1]):
             for x in range(size[0]):
                 if x % 2 == y % 2:
@@ -51,8 +78,28 @@ class Board():
                 else:
                     color = 'black'
 
-                tile = Tile(self.window, color, palette)
+                tile = Tile(self.window, color, palette, (x, y), 60)
                 self.tiles.add(tile)
+
+    def zoom(self, amount):
+        for tile in self.tiles:
+            if amount < 0:
+                if tile.size > 10:
+                    tile.size -= 7
+                else:
+                    tile.size = 10
+            else:
+                if tile.size < 100:
+                    tile.size += 7
+                else:
+                    tile.size = 100
+    def set_tile_size(self, size):
+        for tile in self.tiles:
+            tile.size = size
+
+    def render(self):
+        for tile in self.tiles:
+            tile.render()
 
 
 class Camera():
@@ -97,6 +144,21 @@ class Camera():
 
         self.rect = pyg.rect.Rect(-top, -left, width, height)
 
+
+def terminate():
+    pyg.quit()
+    raise SystemExit()
+
+def get_path(*path):
+    """Returns the full file path of a file."""
+    dirname = os.path.dirname(os.path.abspath(__file__))
+    return os.path.join(dirname, path)
+
+def play_sound(sound):
+    se_channel = pyg.mixer.find_channel()
+
+    se_channel.play(sound)
+
 def main():
     """
         XXX         XXX
@@ -111,8 +173,11 @@ def main():
     """
 
      # Custom Events
-    custom_event = pyg.USEREVENT + 1
+    #custom_event = pyg.USEREVENT + 1
     #pyg.time.set_timer(custom_event, 0)
+
+    board = Board(window)
+    board.generate()
 
     clock = pyg.time.Clock()
 
@@ -130,9 +195,17 @@ def main():
                         terminate()
                     elif event.key == pyg.K_SPACE:
                         ...
+                elif event.type == pyg.MOUSEWHEEL:
+                    board.zoom(event.y)
 
-            camera.follow(player)
-            camera.apply_lens(player, world, world_decor)
+            #camera.follow(player)
+            #camera.apply_lens(player, world, world_decor)
+
+            window.fill((50, 50, 200))
+
+            for tile in board.tiles:
+                tile.update()
+            board.render()
 
             pyg.display.flip()
             clock.tick(FPS)
@@ -147,7 +220,7 @@ def main():
                     elif event.key == pyg.K_BACKQUOTE:
                         terminate()
 
-            window.fill((255, 255, 255))
+            window.fill((100, 100, 200))
 
             pyg.display.flip()
 
@@ -160,6 +233,7 @@ if __name__ == '__main__':
     pyg.init()
     pyg.display.set_caption("ChessRPG")
     window = pyg.display.set_mode(flags=pyg.HWSURFACE | pyg.FULLSCREEN | pyg.DOUBLEBUF)
+    window_rect = window.get_rect()
     WIDTH, HEIGHT = pyg.display.get_window_size()
 
     # Init camera outside of main() so we can access it anywhere
